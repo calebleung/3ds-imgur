e40ef87f61587c86169b0e6afeff4a8ab72a1f8e
Caleb Leung <434c@users.noreply.github.com>
2014-06-12 08:55:26 -0700

    Updated app to be compatible with pyimgur 0.4.0+

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pyimgur import *
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './tmp'
-imgur = pyimgur('CLIENT ID', 'CLIENT SECRET')
+imgur = pyimgur('a117ab112c7028e', 'c81cbe29785a240082659f8f0b462e9343362e13')
 
 @app.route('/', methods=['GET', 'POST'])
 def indexPage():
@@ -19,18 +19,8 @@ def indexPage():
 		except KeyError as e:
 			return render_template('index.html', error = e)
 
-		try:
-			imgurResults = imgur.upload_image_local(filename)
-			imgurLinks = str(imgurResults.json()['data']['link'])
-			imgurLower = imgurLinks.split('imgur.com/')[1].lower()
-		except (KeyError, TypeError):
-			return render_template('index.html', error = True)
-
-		os.remove(filename)
-
 		return render_template('index.html', URLs = imgurLinks, Lower = imgurLower)
 	return render_template('index.html')
 
 if __name__ == "__main__":
-	app.run(host = "127.0.0.1", port=7300)
-
+	app.run(host = "127.0.0.1", port=7301)
